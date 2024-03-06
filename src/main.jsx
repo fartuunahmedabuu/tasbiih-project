b7926e051e0b6c4d39638842bd212a07698fde21
fartuunahmedabuu <fartuunahmed2706@gmail.com>
2024-03-06 13:09:27 +0300

    tasbix

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import "./style.css"
+import Tasbiix from "./Component/Tasbiix"
+ 
+ 
+ 
+ const root = document.getElementById("root");
+
+createRoot(root).render(
+    <Tasbiix/>
+
+)
